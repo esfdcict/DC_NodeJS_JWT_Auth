0b515ea863a4768842733104cf9491add4495d3a
Mike Veilleux <115963834+esfdcict@users.noreply.github.com>
2022-12-19 09:09:42 +0800

    updated doc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DC_NodeJS_JWT_Auth
+
 Authentication with nodejs and Json Web Token (JWT)
+
+## Create IDs
+
+```
+require('crypto').randomBytes(64).toString('hex')
+```
--- server.js
+++ server.js
@@ -1,47 +1,42 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
 const posts = [
-    {
-        username: "Mike",
-        title: "Post 1"
-    },
-    {
-        username: "Kai",
-        title: "Post 2"
-    }
-]
+  {
+    username: "Mike",
+    title: "Post 1",
+  },
+  {
+    username: "Kai",
+    title: "Post 2",
+  },
+];
 
-app.get('/posts', authenticateToken, (req, res) => {
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((p) => p.username === req.user.name));
+});
 
-    res.json(posts.filter(p => p.username === req.user.name));
-})
-
-app.post('/login', (req, res) => {
-
-    const username = req.body.username;
-    const user = { name: username };
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-
-    res.json({ accessToken: accessToken })
-
-})
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user;
-        next();
-    })
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
 app.listen(3000);
